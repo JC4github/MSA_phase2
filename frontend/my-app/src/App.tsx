fe9b06c810906c05a29da15d8b9b7369551d614e
JACE <jacexin77@gmail.com>
2022-07-25 11:37:36 +1200

    basic function finished

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import axios from "axios";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 import './App.css';
+import { Box, Button, Grid, IconButton, Paper, Skeleton } from "@mui/material";
 
 function App() {
+
+    const [animeData, setAnimeData] = useState<undefined | any>(undefined);
+    const [animeName, setAnimeName] = useState("");
+
+    const ANIME_BASE_URL = "https://api.jikan.moe/v4/anime?q="
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Anime Search</h1>
+      <div>
+        <TextField
+          id="search-bar"
+          className='text'
+          value={animeName}
+          onChange={(prop: any) => {
+            setAnimeName(prop.target.value);
+          }}
+          label='Enter a anime name'
+          variant='outlined'
+          placeholder='Search...'
+          size='small'
+          />
+        <IconButton
+          aria-label="search"
+          onClick={()=>{
+            search();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <SearchIcon style={{fill:'blue'}}/>
+        </IconButton>
+
+        {animeData === undefined ? (
+          <p>Anime not found</p>
+        ) : (
+          <div>
+            <h4>{getAnime()?.toString()}</h4>
+            <img src={getAnimeImg()?.toString()} alt="somethings broken" />
+          </div>
+        )}
+      </div>
     </div>
   );
+
+  function search() {
+  axios
+    .get(ANIME_BASE_URL + animeName.toLowerCase() + "&limit=1")
+    .then((res) => {
+      setAnimeData(res.data);
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log("anime not found");
+      setAnimeData(undefined);
+    });
+  }
+
+  function getAnime() {
+    return animeData.data.map((item: any) => item.title); 
+  }
+
+  function getAnimeImg() {
+    return animeData.data.map((item: any) => item.images.jpg.image_url); //issue need fix; index json array
+  }
 }
 
 export default App;
